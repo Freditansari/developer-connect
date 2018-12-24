89277f99344e6e5ea2715ffca9eae0691f471dd4
Freditansari <Freditansari@users.noreply.github.com>
2018-12-24 15:38:06 +0700

    added the route post api user register. about to add the gravatar module

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,33 @@
 const express = require ('express');
+const User = require('../../models/User')
 const router = express.Router();
 
+//@route GET api/users/test
+//@desc tests users router
+//@access public
 router.get('/test', (req, res)=> res.json({msg : "users works"}));
+
+//@route post api/users/register
+//@desc register users
+//@access public
+
+router.post('/register', (req, res)=> {
+    User.findOne({ email : req.body.email})
+    .then(user =>{ 
+        if(user){
+            return res.status(400).json({email: 'Email already exists'})
+        }else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email, 
+                avatar,
+                password: req.body.password
+            });
+
+        }
+    });
+})
+
+
 
 module.exports=router;
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const users = require('./routes/api/users');
 const profiles = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
 
 const db = require('./config/keys').mongoUri;
 
