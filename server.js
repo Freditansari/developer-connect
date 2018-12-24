37aef4930a77ebe5fe772d4e6bb2622c59043e17
Freditansari <Freditansari@users.noreply.github.com>
2018-12-24 18:51:58 +0700

    setup some routes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require ('express');
 const app = express();
 const mongoose = require ('mongoose');
 
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
+
+
 const db = require('./config/keys').mongoUri;
 
 mongoose.connect(db)
@@ -11,6 +16,11 @@ mongoose.connect(db)
 app.get('/', (req, res)=>{
     res.send('hello')
 });
+
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
+
 
 const port = process.env.PORT||5000;
 
