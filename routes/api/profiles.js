ee6e15315d72ebbe845519cadc3b9b5235f7dfa5
Freditansari <Freditansari@users.noreply.github.com>
2018-12-27 04:17:59 +0700

    working on user profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,6 +1,24 @@
 const express = require ('express');
 const router = express.Router();
 
+const mongoose = require ('mongoose');
+const passport = require('passport');
+
+//load profile model 
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
+
 router.get('/test', (req, res)=> res.json({msg : "profile works"}));
+
+//@route get api/profile/
+//@desc get current user profile
+//@access public
+
+router.get('/',  passport.authenticate('jwt', {session: false}), (req, res)=>{});
+
+
+
 
 module.exports=router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -152,6 +152,20 @@ router.get('/current', passport.authenticate('jwt', {session: false}), (req, res
 
 });
 
+router.get('/',  passport.authenticate('jwt', {session: false}), (req, res)=>{
+
+    const errors={};
+    Profile.findOne({user: req.user.id}).then(profile=>{
+        if(!{profile}){
+            errors.noprofile ='there is no profile for this user'
+            return res.status(404).json(errors)
+        }
+        res.json(profile);
+    })
+    .catch(err=> res.status(404).json(err));
+
+});
+
 
 
 
