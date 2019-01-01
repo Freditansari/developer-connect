b202475794e5e371fb916dc8dc9c04de0813cf79
Fredi tansari <freditansari@gmail.com>
2019-01-01 13:14:48 +0700

    added education and experience router and validation

---
+++ validation/education.js
@@ -0,0 +1,41 @@
+const isEmpty = require('./is-empty'); 
+const validator = require('validator');
+
+module.exports= function validateEducationInput(data){
+    let errors={};
+
+    //check if data will be empty, if not it will set the variable to empty
+    //so the validator can check it
+
+    data.school = !isEmpty(data.school) ? data.school: '';
+    data.degree = !isEmpty(data.degree) ? data.degree: '';
+    data.from = !isEmpty(data.from) ? data.from: '';
+    data.fieldOfStudy = !isEmpty(data.fieldOfStudy) ? data.fieldOfStudy: '';
+
+
+    
+
+    if(validator.isEmpty(data.school)){
+        errors.school = 'school is required';
+    }
+
+    if(validator.isEmpty(data.degree)){
+        degree.password = 'degree field is required';
+    }
+    if(validator.isEmpty(data.from)){
+        errors.from = 'from date field is required';
+    }
+    if(validator.isEmpty(data.fieldOfStudy)){
+        errors.fieldOfStudy = 'Field of study is required';
+    }
+
+
+
+    
+
+
+    return {
+        errors, 
+        isValid: isEmpty(errors) 
+    }
+}
---
+++ validation/experience.js
@@ -0,0 +1,36 @@
+const isEmpty = require('./is-empty'); 
+const validator = require('validator');
+
+module.exports= function validateExperienceInput(data){
+    let errors={};
+
+    //check if data will be empty, if not it will set the variable to empty
+    //so the validator can check it
+
+    data.title = !isEmpty(data.title) ? data.title: '';
+    data.company = !isEmpty(data.company) ? data.company: '';
+    data.from = !isEmpty(data.from) ? data.from: '';
+
+    
+
+    if(validator.isEmpty(data.title)){
+        errors.title = 'job title is required';
+    }
+
+    if(validator.isEmpty(data.company)){
+        errors.password = 'company field is required';
+    }
+    if(validator.isEmpty(data.from)){
+        errors.from = 'from date field is required';
+    }
+
+
+
+    
+
+
+    return {
+        errors, 
+        isValid: isEmpty(errors) 
+    }
+}
