3ce999afdad4e444f1df50dcd6167338c8f07f04
Fredi tansari <freditansari@gmail.com>
2019-01-03 10:40:10 +0700

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 
@@ -11,11 +16,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Navbar />
-        <Landing />
-      <Footer />
-      </div>
+      <Router>
+        <div className="App">
+        <Navbar />
+          <Route exact path='/' component ={Landing}></Route>
+          <div className="container">
+          <Route exact path ='/register' component = {Register}></Route>
+          <Route exact path ='/login' component = {Login}></Route>
+
+          </div>
+        <Footer />
+        </div>
+      </Router>
     );
   }
 }
